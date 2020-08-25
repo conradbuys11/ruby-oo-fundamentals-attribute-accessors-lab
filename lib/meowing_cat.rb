b3c49c40ecc34aa8d8112c4c3e2480c9cac9a0f7
conrad_buys <conradbuys11@gmail.com>
2020-08-25 15:56:53 -0400

    pushing changes

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+class Cat
+    def initialize(name = nil)
+        @name = name
+    end
+
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+
+end
